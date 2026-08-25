a0fe9730911932e4d631f02079973eebf8ceddca
srubaiyat <122497388+srubaiyat@users.noreply.github.com>
2026-08-25 12:31:18 -0400

    Update data.js

--- data.js
+++ data.js
@@ -174,7 +174,7 @@ const MEMORIES = [
     category: "travel",
     title: "Kashmir",
     text: "Munez was scared to go into the snow, but not for long with Papa!",
-    images: []
+    images: ["./images/kashmir.png"]
   },
   {
     category: "travel",
